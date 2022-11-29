0de5673b23e7a94059ed8b5a17381dff435e4a6b
Daiana <Daiana@DESKTOP-4N9BAGI>
2022-11-29 15:20:13 +0200

    lab8

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,33 @@
+namespace Todean_Daiana_Lab7;
+using Todean_Daiana_Lab7.Models;
+
+public partial class ListEntryPage : ContentPage
+{
+    public ListEntryPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+}
